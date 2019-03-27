d7d72da49e6a1d85d02aecfba8130a79b1911927
DevUser <1111>
2019-03-27 11:53:04 +0900

    Dropzone Upload

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Upload;
+
+class FileController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'file' => 'required',
+        ]);
+
+        auth()->user()->files()->create([
+            'title' => $request->get('file'),
+        ]);
+
+        //return back()->with('message', 'Your file is submitted Successfully');
+    }
+
+    public function upload(Request $request)
+    {
+
+        $uploadedFile = $request->file('file');
+        $filename = time().$uploadedFile->getClientOriginalName();
+
+        Storage::disk('local')->putFileAs(
+            'files/',
+            $uploadedFile,
+            $filename
+        );
+
+        $upload = new Upload;
+        $upload->filename = $filename;
+
+        $upload->user()->associate(auth()->user());
+
+        $upload->save();
+
+        return response()->json([
+            'id' => $upload->id
+        ]);
+    }
+}
