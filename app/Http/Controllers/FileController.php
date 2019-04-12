17512dc385a8c6b585374c3f4af5c5413513606c
DevUser <1111>
2019-04-12 14:55:04 +0600

    Compare Prices

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -13,18 +13,17 @@ class FileController extends Controller
 {
     public function parse(Request $request)
     {
-        try
-        {
-            $request->validate([
-                'file_toParse'=>'required'
-            ]);
+        $request->validate([
+            'file_toParse'=>'required'
+        ]);
 
-            $filename = Storage::disk('local')->getDriver()->getAdapter()->applyPathPrefix('files/'.$request->get('file_toParse'));
+        $filename = Storage::disk('local')->getDriver()->getAdapter()->applyPathPrefix('files/'.$request->get('file_toParse'));
 
-            if (!$filename || !file_exists($filename)) {
-                return back()->with('message', 'Your file is Not submitted Successfully');
-            }
-            exec("\"C:\Program Files\gs\gs9.26\bin\gswin64c.exe\" -dSAFER -sDEVICE=png16m -dINTERPOLATE -dNumRenderingThreads=8 -r400 -o ".pathinfo($filename,PATHINFO_DIRNAME)."/".pathinfo($filename,PATHINFO_FILENAME)."%d.png -c 30000000 setvmthreshold -f ".$filename);
+        if (!$filename || !file_exists($filename)) {
+            return back()->with('message', 'Your file is Not submitted Successfully');
+        }
+
+        exec("\"C:\Program Files\gs\gs9.26\bin\gswin64c.exe\" -dSAFER -sDEVICE=png16m -dINTERPOLATE -dNumRenderingThreads=8 -r400 -o ".pathinfo($filename,PATHINFO_DIRNAME)."/".pathinfo($filename,PATHINFO_FILENAME)."%d.png -c 30000000 setvmthreshold -f ".$filename);
 //            $gs=new Ghostscript();
 //            Ghostscript::setGsPath('C:\Program Files\gs\gs9.26\bin\gswin64c.exe');
 //            $gs->setDevice('png')
@@ -34,32 +33,26 @@ class FileController extends Controller
 //                ->setTextAntiAliasing(Ghostscript::ANTIALIASING_HIGH);
 //            //$gs->getDevice()->setQuality(300);
 //            $gs->render();
-            $files = glob(pathinfo($filename,PATHINFO_DIRNAME)."/".pathinfo($filename,PATHINFO_FILENAME)."*.png");
-            $text="";
-            $tessAPI=new TesseractOCR();
+        $files = glob(pathinfo($filename,PATHINFO_DIRNAME)."/".pathinfo($filename,PATHINFO_FILENAME)."*.png");
+        $text="";
+        $tessAPI=new TesseractOCR();
 //            $tessAPI->executable('/path/to/tesseract')
-            $tessAPI->lang('rus','eng');
-            foreach ($files as $file)
-            {
-                $text.=$tessAPI->image($file)
-                    ->run();
-                unlink($file);
-            }
-            foreach ((explode("\n", $text)) as $line){
-                $price= Prices::whereRaw(" '".$line."' % \"priceKZT\"")->get();
-            }
-            dd($price);
-            auth()->user()->files()->create([
-                'filename'=>$request->get('file_toParse')
-            ]);
-
-            return back()->with('message', 'Your file is submitted Successfully');
-        }
-        catch (\Exception $e)
+        $tessAPI->lang('rus','eng');
+        foreach ($files as $file)
         {
-            dd($e);
+            $text.=$tessAPI->image($file)
+                ->run();
+            unlink($file);
         }
+        foreach ((explode("\n", $text)) as $line){
+            $price= Prices::whereRaw(" '".$line."' % \"priceKZT\"")->get();
+        }
+        dd($price);
+        auth()->user()->files()->create([
+            'filename'=>$request->get('file_toParse')
+        ]);
 
+        return back()->with('message', 'Your file is submitted Successfully');
     }
 
     public function upload(Request $request)
